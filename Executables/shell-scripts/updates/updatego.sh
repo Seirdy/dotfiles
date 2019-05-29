96653905975e6d0c05e98338b56fd862a519cb99
Rohan Kumar <seirdy@protonmail.com>
2019-05-29 23:39:46 +0530

    Install fast.com cli
    Install github.com/ddo/fast for speed testing. Make it update with
    the rest of my go packages.

--- Executables/shell-scripts/updates/updatego.sh
+++ Executables/shell-scripts/updates/updatego.sh
@@ -1,3 +1,6 @@
 #!/bin/dash
 go get -u -v github.com/cjbassi/gotop
 go get -u -v github.com/mattn/efm-langserver/cmd/efm-langserver
+go get -u -v github.com/Code-Hex/Neo-cowsay/cmd/cowsay
+go get -u -v github.com/Code-Hex/Neo-cowsay/cmd/cowthink
+go get -u -v github.com/ddo/fast
