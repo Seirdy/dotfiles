8d39a7f8f2f064b117a998fb00ae7554f2632c37
Rohan Kumar <seirdy@protonmail.com>
2019-06-19 18:01:08 -0700

    Improve update-all
    - Use lowercase local variable names
    - Don't unnecessarily time full-update when each update is already timed
    - Move update-go's stderr to stdout for better verbose output.

--- Executables/shell-scripts/updates/update-go.sh
+++ Executables/shell-scripts/updates/update-go.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env dash
 
-go get -u -v github.com/cjbassi/gotop
-go get -u -v github.com/mattn/efm-langserver/cmd/efm-langserver
-go get -u -v github.com/Code-Hex/Neo-cowsay/cmd/cowsay
-go get -u -v github.com/Code-Hex/Neo-cowsay/cmd/cowthink
-go get -u -v github.com/ddo/fast
-go get -u -v golang.org/x/tour
+go_update() {
+	go get -u -v "$@" 2>&1  # verbose output is sent to stderr for some reason
+}
+
+go_update github.com/cjbassi/gotop
+go_update github.com/mattn/efm-langserver/cmd/efm-langserver
+go_update github.com/Code-Hex/Neo-cowsay/cmd/cowsay
+go_update github.com/Code-Hex/Neo-cowsay/cmd/cowthink
+go_update github.com/ddo/fast
+go_update golang.org/x/tour
