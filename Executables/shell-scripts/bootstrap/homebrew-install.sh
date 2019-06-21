95421307a06ee5a8563227b320bc2cdefcc5315d
Rohan Kumar <seirdy@protonmail.com>
2019-06-20 17:23:14 -0700

    Update bootstrap files

---
+++ Executables/shell-scripts/bootstrap/homebrew-install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+brew install abduco asciinema aria2 bash calc catimg cmake cmatrix coreutils curl dante dash docker exiftool ext4fuse ffmpeg gnupg go htop imagemagick lua luajit luarocks ncurses neovim nnn node opus p7zip pandoc pandoc-citeproc pandoc-crossref pcre pkg-config python redshift rust sdl sdl2 shellcheck socksify tmux w3m wget x264 x265 yadm zsh
+
+brew cask install firefox-nightly font-hasklig font-noto-nerd-font-mono font-noto-color-emoji font-source-sans-pro font-source-serif-pro gimp icanhazshortcut keepassxc kitty mactex-no-gui mpv osxfuse signal-beta vimr
---
+++ Executables/shell-scripts/bootstrap/python.sh
@@ -0,0 +1,30 @@
+#!/bin/dash
+
+pip3 install -U --user pynvim pipx
+
+pipx install black
+pipx install ddgr
+pipx install flake8
+pipx install pipx install gcalcli  # Eww, Google Apps
+pipx install glances
+pipx install haxor-news
+pipx install httpie
+pipx install mypy
+pipx install neovim-remote
+pipx install pockyt
+pipx install proselint
+pipx install pydiatra
+pipx install requires.io
+pipx install restructuredtext-lint
+pipx install rtv  # Discontinued. TODO: see if community fork lives.
+pipx install safety
+pipx install scspell3k
+pipx install standardebooks
+pipx install thefuck
+pipx install tox
+pipx install trash-cli
+pipx install twine
+pipx install urlscan
+pipx install virtualenv
+pipx install xonsh
+pipx install youtube-dl
