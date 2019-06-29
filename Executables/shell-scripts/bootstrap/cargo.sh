f1bec7e3eddd3486d7a7d17b4fcdf3a8c757622f
Rohan Kumar <seirdy@protonmail.com>
2019-06-28 21:46:43 -0700

    Use "mdcat" rust package to view markdown files in terminal

---
+++ Executables/shell-scripts/bootstrap/cargo.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env dash
+
+cargo install bat cargo cargo-update exa fd-find ffsend hyperfine lsd onefetch ripgrep rls shellharden tealdeer
+cargo install --git https://github.com/jameslzhu/roflcat --all-features
+cargo install --git https://github.com/lunaryorn/mdcat --all-features
+tldr --update
