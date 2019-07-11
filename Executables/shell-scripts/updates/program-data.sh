6b582b6c8db65ea597f4a29d270314ef511046df
Rohan Kumar <seirdy@protonmail.com>
2019-07-11 16:48:18 -0700

    Add updating program data to update-all script

---
+++ Executables/shell-scripts/updates/program-data.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env dash
+
+start_time=$(date '+%s')
+
+echo 'tldr update'
+tldr --update
+echo 'Sync Pocket and Buku'
+poku -c ~/.config/poku.cfg
+echo 'Update buku database'
+buku -u --threads 10
+echo 'Updating RSS feeds'
+newsboat -x reload
+
+end_time=$(date '+%s')
+elapsed=$(echo "$end_time - $start_time" | bc)
+echo "Time elapsed: $elapsed seconds"
