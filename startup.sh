a564c2240e87911d735ed958c30edbc1e8e54e05
Rohan Kumar <seirdy@protonmail.com>
2019-07-18 07:20:01 -0700

    First steps in switching to Sway

--- startup.sh
+++ startup.sh
@@ -26,7 +26,7 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
 
 export LC_TIME=en_ZA.UTF-8
-export DEFTERM="kitty -1" # Default terminal
+export TERMINAL="kitty -1" # Default terminal
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 export NVIM_GTK_PREFER_DARK_THEME=1
