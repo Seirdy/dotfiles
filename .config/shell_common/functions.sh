07aec52fa68b3714195e05c68541111610de1ea9
Rohan Kumar <seirdy@protonmail.com>
2019-08-20 19:23:55 -0700

    Remove transfer.sh function

--- .config/shell_common/functions.sh
+++ .config/shell_common/functions.sh
@@ -17,23 +17,6 @@ wdir() {
 	cd "$PROGDIR" || return
 }
 
-# transfer.sh
-transfer() {
-	if [ $# -eq 0 ]; then
-		printf "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md\n"
-		return 1
-	fi
-	tmpfile=$(mktemp -t transferXXX)
-	if tty -s; then
-		basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
-		curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >>"$tmpfile"
-	else
-		curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >>"$tmpfile"
-	fi
-	cat "$tmpfile"
-	rm -f "$tmpfile"
-}
-
 n() {
 	nnn "$@"
 
