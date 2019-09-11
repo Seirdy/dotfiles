15a8e664477b6581adae8306925f9f5d797652e1
Rohan Kumar <seirdy@protonmail.com>
2019-09-10 22:16:19 -0700

    Bootstrap: new pipx packages

--- Executables/shell-scripts/bootstrap/python.sh
+++ Executables/shell-scripts/bootstrap/python.sh
@@ -2,7 +2,9 @@
 
 pip3 install -U --user pynvim pipx
 
+pipx install beets &
 pipx install black &
+pipx install buku &
 pipx install consolemd &
 pipx install ddgr &
 pipx install flake8 &
@@ -12,6 +14,8 @@ pipx install haxor-news &
 pipx install httpie &
 pipx install mypy &
 pipx install myqr &
+# curses mpd client
+pipx install ncmpy &
 pipx install neovim-remote &
 pipx install pockyt &
 pipx install proselint &
@@ -19,16 +23,19 @@ pipx install pydiatra &
 pipx install pydocstyle &
 pipx install pygments &
 pipx install pyre-check &
+pipx install pytest &
 pipx install requires.io &
 pipx install restructuredtext-lint &
-pipx install tuir &
 pipx install safety &
 pipx install scspell3k &
+pipx install sphinx &
 pipx install standardebooks &
 pipx install thefuck &
 pipx install tox &
 pipx install trash-cli &
+pipx install tuir &
 pipx install twine &
+pipx install ueberzug &
 pipx install urlscan &
 pipx install virtualenv &
 pipx install xenon &
