01a7560106f526553cc5a52df1a42118496862ed
Rohan Kumar <seirdy@protonmail.com>
2019-09-11 20:07:59 -0700

    Switch from npm to pnpm

--- Executables/shell-scripts/updates/node.sh
+++ Executables/shell-scripts/updates/node.sh
@@ -2,11 +2,13 @@
 
 start_time=$(date '+%s')
 
-npm i -g tree-sitter
-npm i -g tree-sitter-bash
-npm i -g bash-language-server
-npm i -g markdownlint-cli
-npm i -g neovim
+# install pnpm if it doesn't already exist,
+# and erase the now-obsolete $NODE_PATH/lib directory.
+command -v pnpm >/dev/null || npm add -g pnpm && rm -rf "${NODE_PATH:?}/lib"
+pnpm add -g pnpm
+pnpm add -g bash-language-server
+pnpm add -g markdownlint-cli
+pnpm add -g neovim
 
 end_time=$(date '+%s')
 elapsed=$(echo "$end_time - $start_time" | bc)
