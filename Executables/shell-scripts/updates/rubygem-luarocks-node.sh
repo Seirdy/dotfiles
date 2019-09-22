0864de1db64ca2f125cce9404a2f8e2a94634a9a
Rohan Kumar <seirdy@protonmail.com>
2019-09-21 18:57:19 -0700

    update-all: npm+rubygem+luarocks updates
    All three of these update very quickly, especially since I switched from
    npm to pnpm; they should be combined.

--- Executables/shell-scripts/updates/rubygem-luarocks-node.sh
+++ Executables/shell-scripts/updates/rubygem-luarocks-node.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env dash
-
 start_time=$(date '+%s')
 
+echo "===UPDATE: Upgrading: Lua======="
+luarocks install lcf --local
+luarocks install --server=http://luarocks.org/dev lua-lsp --local
+echo "===UPDATE: Upgrading: Rubygems======="
+gem update --prerelease -V
+echo "===UPDATE: Upgrading: npm packages"
 # install pnpm if it doesn't already exist,
 # and erase the now-obsolete $NODE_PATH/lib directory.
 command -v pnpm >/dev/null || npm add -g pnpm && rm -rf "${NODE_PATH:?}/lib"
