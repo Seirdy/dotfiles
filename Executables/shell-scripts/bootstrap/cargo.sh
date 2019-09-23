1449e28b00384d16ce27d9da282e56e8fb50ef65
Rohan Kumar <seirdy@protonmail.com>
2019-09-22 18:11:22 -0700

    Installed many more programs

--- Executables/shell-scripts/bootstrap/cargo.sh
+++ Executables/shell-scripts/bootstrap/cargo.sh
@@ -4,14 +4,16 @@ cargo install cargo cargo-update rls
 cargo_install_git() {
 	cargo install --git "$@" --force
 }
-cargo_install_git https://github.com/BurntSushi/ripgrep
+cargo_install_git https://github.com/BurntSushi/ripgrep.git
 cargo_install_git https://github.com/Peltoche/lsd.git
 cargo_install_git https://github.com/anordal/shellharden.git
 cargo_install_git https://github.com/dbrgn/tealdeer.git
 cargo_install_git https://github.com/jameslzhu/roflcat.git
+cargo_install_git https://github.com/Y2Z/monolith.git
 cargo_install_git https://github.com/lunaryorn/mdcat.git
 cargo_install_git https://github.com/o2sh/onefetch.git
 cargo_install_git https://github.com/ogham/exa.git
+cargo_install_git https://github.com/chmln/sd.git
 cargo_install_git https://github.com/sharkdp/bat.git
 cargo_install_git https://github.com/sharkdp/fd.git
 cargo_install_git https://github.com/sharkdp/hyperfine.git
