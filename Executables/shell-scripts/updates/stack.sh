0d40b02a3e00ea452afc53e653dbf3030e89474a
Rohan Kumar <seirdy@protonmail.com>
2019-10-18 05:21:39 -0700

    update-all: try stack nightly resolver
    Attempt to use the nightly stack resolver before falling back to
    whatever the project's local STACK_YAML specifies.

--- Executables/shell-scripts/updates/stack.sh
+++ Executables/shell-scripts/updates/stack.sh
@@ -21,17 +21,24 @@ stack_install_git_nightly() {
 		&& STACK_YAML="./stack.yaml" stack_local --resolver=nightly install
 }
 
+stack_install_nightly_fallback() {
+	stack_install_git_nightly "$1" \
+		|| {
+			echo "Failed to update with nightly resolver; trying with specified resolver" && stack_install_git "$1"
+		}
+}
+
 stack_local config set resolver nightly
 stack_local upgrade
 stack_local update
 
 # Pandoc and friends
-stack_install_git https://github.com/jgm/pandoc.git
+stack_install_nightly_fallback https://github.com/jgm/pandoc.git
 # Pandoc filters
-stack_install_git https://github.com/jgm/pandoc-citeproc.git
-stack_install_git https://github.com/owickstrom/pandoc-include-code.git
-stack_install_git https://github.com/lierdakil/pandoc-crossref.git
-stack_install_git https://github.com/owickstrom/pandoc-emphasize-code.git
+stack_install_nightly_fallback https://github.com/jgm/pandoc-citeproc.git
+stack_install_nightly_fallback https://github.com/owickstrom/pandoc-include-code.git
+stack_install_nightly_fallback https://github.com/lierdakil/pandoc-crossref.git
+stack_install_nightly_fallback https://github.com/owickstrom/pandoc-emphasize-code.git
 
 # Shell script linter
 stack_install_git https://github.com/koalaman/shellcheck.git
@@ -40,9 +47,9 @@ ghq_get_cd https://github.com/digital-asset/ghcide.git \
 	&& STACK_YAML="./stack88.yaml" stack_local setup \
 	&& STACK_YAML="./stack88.yaml" stack_local install
 # source code grepper
-stack_install_git_nightly https://github.com/awgn/cgrep.git
+stack_install_nightly_fallback https://github.com/awgn/cgrep.git
 # Dockerfile linter
-stack_install_git https://github.com/hadolint/hadolint
+stack_install_nightly_fallback https://github.com/hadolint/hadolint
 
 end_time=$(date '+%s')
 elapsed=$(echo "$end_time - $start_time" | bc)
