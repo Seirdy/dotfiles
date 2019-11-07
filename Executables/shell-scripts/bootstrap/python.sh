758fe603c8a7583209401bd42f5ff73ae4ac3c8d
Rohan Kumar <seirdy@protonmail.com>
2019-11-07 13:59:43 -0800

    Get pipx packages from git master

--- Executables/shell-scripts/bootstrap/python.sh
+++ Executables/shell-scripts/bootstrap/python.sh
@@ -2,25 +2,26 @@
 
 pip3 install -U --user pynvim pipx
 
+# curses mpd client
 pipx install beets &
 pipx install black &
-pipx install buku &
+pipx install buku --spec git+https://github.com/jarun/Buku.git &
 pipx install consolemd &
-pipx install ddgr &
+pipx install ddgr --spec git+https://github.com/jarun/ddgr.git &
 pipx install flake8 &
 pipx install gcalcli &
 pipx install glances &
-pipx install haxor-news &
-pipx install httpie &
+pipx install haxor-news --spec git+https://github.com/donnemartin/haxor-news.git &
+pipx install howmanypeoplearearound --spec git+https://github.com/schollz/howmanypeoplearearound.git &
+pipx install httpie --spec git+https://github.com/jakubroztocil/httpie.git &
 pipx install mypy &
 pipx install myqr &
-# curses mpd client
 pipx install ncmpy &
 pipx install neovim-remote &
-pipx install pockyt &
-pipx install poku &
+pipx install pockyt --spec git+https://github.com/arvindch/pockyt.git &
+pipx install poku --spec git+https://github.com/shanedabes/poku.git &
 pipx install proselint &
-pipx install ptpython &
+pipx install ptpython --spec git+https://github.com/prompt-toolkit/ptpython.git &
 pipx install pydiatra &
 pipx install pydocstyle &
 pipx install pygments &
@@ -30,17 +31,19 @@ pipx install requires.io &
 pipx install restructuredtext-lint &
 pipx install safety &
 pipx install scspell3k &
-pipx install speedtest-cli &
+pipx install speedtest-cli --spec git+https://github.com/sivel/speedtest-cli.git &
 pipx install sphinx &
-pipx install standardebooks &
-pipx install thefuck &
+pipx install standardebooks --spec git+https://github.com/standardebooks/tools.git &
+pipx install thefuck --spec git+https://github.com/nvbn/thefuck.git &
 pipx install tox &
-pipx install trash-cli &
-pipx install tuir &
+pipx install trash-cli --spec git+https://github.com/andreafrancia/trash-cli.git &
+pipx install tuir --spec git+https://gitlab.com/ajak/tuir.git &
 pipx install twine &
 pipx install ueberzug &
-pipx install urlscan &
-pipx install virtualenv &
-pipx install xenon &
-pipx install xonsh &
-pipx install youtube-dl
+pipx install unimatrix
+pipx install unimatrix --spec git+https://github.com/will8211/unimatrix.git
+pipx install urlscan --spec git+https://github.com/firecat53/urlscan.git
+pipx install virtualenv
+pipx install xenon
+pipx install xonsh
+pipx install youtube-dl --spec git+https://github.com/ytdl-org/youtube-dl.git
--- Executables/shell-scripts/updates/pip-pipx.sh
+++ Executables/shell-scripts/updates/pip-pipx.sh
@@ -4,6 +4,22 @@ start_time=$(date '+%s')
 
 pip3 install -U --user pynvim pipx
 pipx upgrade-all
+pipx upgrade buku --spec git+https://github.com/jarun/Buku.git
+pipx upgrade ddgr --spec git+https://github.com/jarun/ddgr.git
+pipx upgrade haxor-news --spec git+https://github.com/donnemartin/haxor-news.git
+pipx upgrade httpie --spec git+https://github.com/jakubroztocil/httpie.git
+pipx upgrade https://github.com/will8211/unimatrix --spec git+unimatrix.git
+pipx upgrade pockyt --spec git+https://github.com/arvindch/pockyt.git
+pipx upgrade poku --spec git+https://github.com/shanedabes/poku.git
+pipx upgrade ptpython --spec git+https://github.com/prompt-toolkit/ptpython.git
+pipx upgrade speedtest-cli --spec git+https://github.com/sivel/speedtest-cli.git
+pipx upgrade standardebooks --spec git+https://github.com/standardebooks/tools.git
+pipx upgrade thefuck --spec git+https://github.com/nvbn/thefuck.git
+pipx upgrade trash-cli --spec git+https://github.com/andreafrancia/trash-cli.git
+pipx upgrade tuir --spec git+https://gitlab.com/ajak/tuir.git
+pipx upgrade urlscan --spec git+https://github.com/firecat53/urlscan.git
+pipx upgrade youtube-dl --spec git+https://github.com/ytdl-org/youtube-dl.git
+pipx upgrade howmanypeoplearearound --spec git+https://github.com/schollz/howmanypeoplearearound.git
 
 end_time=$(date '+%s')
 elapsed=$(echo "$end_time - $start_time" | bc)
