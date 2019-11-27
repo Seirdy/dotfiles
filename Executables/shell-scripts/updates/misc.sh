4e16cbf50f842fb1ddc07e703b362aba36e40e43
Rohan Kumar <seirdy@protonmail.com>
2019-11-26 20:54:52 -0800

    Use cantata-dynamic perl script
    I use the cantata-dynamic perl script to dynamize playlists. Dynamic
    playlists are generated according to rules, and automatically delete
    played songs and add new ones.

--- Executables/shell-scripts/updates/misc.sh
+++ Executables/shell-scripts/updates/misc.sh
@@ -10,9 +10,12 @@ ghq_get_cd https://github.com/poljar/weechat-matrix.git && . ./venv/bin/activate
 	&& pip3 install -U requirements.txt \
 	&& make install
 
-ghq get -u https://github.com/keith/edit-weechat.git
+# these repos have items symlinked to other places
 
+ghq get -u https://github.com/keith/edit-weechat.git
 ghq get -u https://github.com/carnager/clerk.git
+# I use this repo's "cantata-dynamic" perl script to generate playlists
+ghq get -u https://github.com/CDrummond/cantata.git
 
 end_time=$(date '+%s')
 elapsed=$(echo "$end_time - $start_time" | bc)
