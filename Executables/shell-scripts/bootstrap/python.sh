0d0d0f8e6efad6772a200d36f3ada3b38e401707
Rohan Kumar <seirdy@protonmail.com>
2019-12-17 12:26:30 -0800

    Use the new metadata features of pipx
    Pipx updates now update from vcs is that's how packages were initially
    installed; `--spec` is removed for updates. See
    https://github.com/pipxproject/pipx/commit/ea98438ba7d17bab47604ba6a9fef6189f46f1a4
    and https://github.com/pipxproject/pipx/pull/222

--- Executables/shell-scripts/bootstrap/python.sh
+++ Executables/shell-scripts/bootstrap/python.sh
@@ -6,21 +6,21 @@ pip3 install -U --user pynvim pipx
 pipx install beets &
 pipx install black &
 pipx install buku --spec git+https://github.com/jarun/Buku.git &
-pipx install consolemd &
+pipx install consolemd --spec git+https://github.com/kneufeld/consolemd.git &
 pipx install ddgr --spec git+https://github.com/jarun/ddgr.git &
 pipx install emoji-fzf --spec git+https://github.com/noahp/emoji-fzf.git
 pipx install flake8 &
 pipx install gcalcli &
-pipx install glances &
+pipx install glances --spec git+https://github.com/nicolargo/glances.git &
 pipx install haxor-news --spec git+https://github.com/donnemartin/haxor-news.git &
 pipx install howmanypeoplearearound --spec git+https://github.com/schollz/howmanypeoplearearound.git &
 pipx install httpie --spec git+https://github.com/jakubroztocil/httpie.git &
 pipx install mypy &
-pipx install myqr &
-pipx install ncmpy &
+pipx install myqr --spec git+https://github.com/sylnsfar/qrcode.git &
+pipx install ncmpy --spec git+https://github.com/cykerway/ncmpy.git &
 pipx install neovim-remote &
 pipx install pockyt --spec git+https://github.com/arvindch/pockyt.git &
-pipx install podman-compose
+pipx install podman-compose --spec git+https://github.com/containers/podman-compose.git &
 pipx install poku --spec git+https://github.com/shanedabes/poku.git &
 pipx install proselint &
 pipx install ptpython --spec git+https://github.com/prompt-toolkit/ptpython.git &
@@ -42,10 +42,10 @@ pipx install trash-cli --spec git+https://github.com/andreafrancia/trash-cli.git
 pipx install tuir --spec git+https://gitlab.com/ajak/tuir.git &
 pipx install twine &
 pipx install ueberzug &
-pipx install unimatrix
 pipx install unimatrix --spec git+https://github.com/will8211/unimatrix.git
-pipx install urlscan --spec git+https://github.com/firecat53/urlscan.git
-pipx install virtualenv
-pipx install xenon
-pipx install xonsh
+pipx install urlscan --spec git+https://github.com/firecat53/urlscan.git &
+pipx install virtualenv &
+pipx install xenon &
+pipx install xonsh --spec git+https://github.com/xonsh/xonsh.git
 pipx install youtube-dl --spec git+https://github.com/ytdl-org/youtube-dl.git
+pipx install mercurial
--- Executables/shell-scripts/updates/pip-pipx.sh
+++ Executables/shell-scripts/updates/pip-pipx.sh
@@ -2,25 +2,9 @@
 
 start_time=$(date '+%s')
 
-pip3 install -U --user pynvim pipx
+pip3 install -U --user git+https://github.com/pipxproject/pipx.git
+pip3 install -U --user pynvim
 pipx upgrade-all
-pipx upgrade buku --spec git+https://github.com/jarun/Buku.git
-pipx upgrade ddgr --spec git+https://github.com/jarun/ddgr.git
-pipx upgrade emoji-fzf --spec git+https://github.com/noahp/emoji-fzf.git
-pipx upgrade haxor-news --spec git+https://github.com/donnemartin/haxor-news.git
-pipx upgrade howmanypeoplearearound --spec git+https://github.com/schollz/howmanypeoplearearound.git
-pipx upgrade httpie --spec git+https://github.com/jakubroztocil/httpie.git
-pipx upgrade unimatrix --spec git+https://github.com/will8211/unimatrix.git
-pipx upgrade pockyt --spec git+https://github.com/arvindch/pockyt.git
-pipx upgrade poku --spec git+https://github.com/shanedabes/poku.git
-pipx upgrade ptpython --spec git+https://github.com/prompt-toolkit/ptpython.git
-pipx upgrade speedtest-cli --spec git+https://github.com/sivel/speedtest-cli.git
-pipx upgrade standardebooks --spec git+https://github.com/standardebooks/tools.git
-pipx upgrade thefuck --spec git+https://github.com/nvbn/thefuck.git
-pipx upgrade trash-cli --spec git+https://github.com/andreafrancia/trash-cli.git
-pipx upgrade tuir --spec git+https://gitlab.com/ajak/tuir.git
-pipx upgrade urlscan --spec git+https://github.com/firecat53/urlscan.git
-pipx upgrade youtube-dl --spec "$GHQ_ROOT/github.com/ytdl-org/youtube-dl"
 
 end_time=$(date '+%s')
 elapsed=$(echo "$end_time - $start_time" | bc)
