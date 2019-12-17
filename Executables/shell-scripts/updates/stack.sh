a5323d0b345c5eb8404bf82fe0444c081b30d9c4
Rohan Kumar <seirdy@protonmail.com>
2019-12-17 10:54:18 -0800

    System update: parallelize/optimize stack builds

--- Executables/shell-scripts/updates/stack.sh
+++ Executables/shell-scripts/updates/stack.sh
@@ -4,34 +4,59 @@ start_time=$(date '+%s')
 
 # shellcheck source=../../../.config/shell_common/functions_ghq.sh
 . "$HOME/.config/shell_common/functions_ghq.sh"
+# shellcheck source=./cc_funcs.sh
+. "$HOME/Executables/shell-scripts/updates/cc_funcs.sh"
 
 stack_local() {
 	stack --local-bin-path="$HOME/Executables/stack/bin" "$@"
 }
 
+if [ "$threads" -gt 4 ]; then
+	echo "using multiple threads"
+	ghc_opts="-O2 -optc-O3 -threaded -j$threads +RTS -A32m -RTS"
+else
+	ghc_opts='-O2 -optc-O3 -threaded +RTS -A32m -RTS'
+fi
+
+stack_local_optimized() {
+	stack_local "$@" --ghc-options="$ghc_opts"
+}
+
 stack_install_git() {
-	ghq_get_cd "$1" \
-		&& STACK_YAML="./stack.yaml" stack_local setup \
-		&& STACK_YAML="./stack.yaml" stack_local install
+	ghq_get_cd "$1" || return 1
+	if [ -f ./stack-nightly.yaml ]; then
+		STACK_YAML=./stack-nightly.yaml
+	elif [ -f ./stack.yaml ]; then
+		STACK_YAML=./stack.yaml
+	fi
+	export STACK_YAML
+	stack_local setup \
+		&& stack_local_optimized install
 }
 
 stack_install_git_nightly() {
-	ghq_get_cd "$1" \
-		&& STACK_YAML="./stack.yaml" stack_local --resolver=nightly setup \
-		&& STACK_YAML="./stack.yaml" stack_local --resolver=nightly install
+	ghq_get_cd "$1" || return 1
+	if [ -f ./stack-nightly.yaml ]; then
+		STACK_YAML=./stack-nightly.yaml
+	elif [ -f ./stack.yaml ]; then
+		STACK_YAML=./stack.yaml
+	fi
+	export STACK_YAML
+	stack_local --resolver=nightly setup \
+		&& stack_local_optimized --resolver=nightly install
 }
 
 stack_install_nightly_fallback() {
 	stack_install_git_nightly "$1" \
 		|| {
-			echo "Failed to update with nightly resolver; trying with specified resolver" \
+			echo 'Failed to update with nightly resolver; trying with specified resolver' \
 				&& stack_install_git "$1"
 		}
 }
 
-stack_local config set resolver nightly
-stack_local upgrade
+# update hackage data
 stack_local update
+stack_install_nightly_fallback https://github.com/commercialhaskell/stack.git
 
 # Pandoc and friends
 stack_install_nightly_fallback https://github.com/jgm/pandoc.git
@@ -50,6 +75,7 @@ stack_install_nightly_fallback https://github.com/hadolint/hadolint.git
 ghq_get_cd https://github.com/digital-asset/ghcide.git \
 	&& STACK_YAML="./stack88.yaml" stack_local setup \
 	&& STACK_YAML="./stack88.yaml" stack_local install
+
 # source code grepper
 stack_install_nightly_fallback https://github.com/awgn/cgrep.git
 
