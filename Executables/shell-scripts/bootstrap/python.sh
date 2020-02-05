0d60cc528ec8358b7dffac1aff3f0fe776d74c5f
Rohan Kumar <seirdy@protonmail.com>
2020-02-05 03:05:06 -0800

    Pipx bootstrap script: remove package names
    Package names are no longer needed when installing with a spec.

--- Executables/shell-scripts/bootstrap/python.sh
+++ Executables/shell-scripts/bootstrap/python.sh
@@ -5,25 +5,22 @@ pip3 install -U --user pynvim pipx
 # curses mpd client
 pipx install beets &
 pipx install black &
-pipx install buku --spec git+https://github.com/jarun/Buku.git &
-pipx install consolemd --spec git+https://github.com/kneufeld/consolemd.git &
-pipx install ddgr --spec git+https://github.com/jarun/ddgr.git &
-pipx install emoji-fzf --spec git+https://github.com/noahp/emoji-fzf.git
+pipx install git+https://github.com/jarun/Buku.git &
+pipx install git+https://github.com/kneufeld/consolemd.git &
+pipx install git+https://github.com/jarun/ddgr.git &
+pipx install git+https://github.com/noahp/emoji-fzf.git
 pipx install flake8 &
-pipx install gcalcli &
-pipx install glances --spec git+https://github.com/nicolargo/glances.git &
-pipx install haxor-news --spec git+https://github.com/donnemartin/haxor-news.git &
-pipx install howmanypeoplearearound --spec git+https://github.com/schollz/howmanypeoplearearound.git &
-pipx install httpie --spec git+https://github.com/jakubroztocil/httpie.git &
+pipx install git+https://github.com/nicolargo/glances.git &
+pipx install git+https://github.com/donnemartin/haxor-news.git &
+pipx install git+https://github.com/jakubroztocil/httpie.git &
 pipx install mypy &
-pipx install myqr --spec git+https://github.com/sylnsfar/qrcode.git &
-pipx install ncmpy --spec git+https://github.com/cykerway/ncmpy.git &
+pipx install git+https://github.com/cykerway/ncmpy.git &
 pipx install neovim-remote &
-pipx install pockyt --spec git+https://github.com/arvindch/pockyt.git &
-pipx install podman-compose --spec git+https://github.com/containers/podman-compose.git &
-pipx install poku --spec git+https://github.com/shanedabes/poku.git &
+pipx install git+https://github.com/arvindch/pockyt.git &
+pipx install git+https://github.com/containers/podman-compose.git &
+pipx install git+https://github.com/shanedabes/poku.git &
 pipx install proselint &
-pipx install ptpython --spec git+https://github.com/prompt-toolkit/ptpython.git &
+pipx install git+https://github.com/prompt-toolkit/ptpython.git &
 pipx install pydiatra &
 pipx install pydocstyle &
 pipx install pygments &
@@ -33,19 +30,18 @@ pipx install requires.io &
 pipx install restructuredtext-lint &
 pipx install safety &
 pipx install scspell3k &
-pipx install speedtest-cli --spec git+https://github.com/sivel/speedtest-cli.git &
+pipx install git+https://github.com/sivel/speedtest-cli.git &
 pipx install sphinx &
-pipx install standardebooks --spec git+https://github.com/standardebooks/tools.git &
-pipx install thefuck --spec git+https://github.com/nvbn/thefuck.git &
+pipx install git+https://github.com/standardebooks/tools.git &
+pipx install git+https://github.com/nvbn/thefuck.git &
 pipx install tox &
-pipx install trash-cli --spec git+https://github.com/andreafrancia/trash-cli.git &
-pipx install tuir --spec git+https://gitlab.com/ajak/tuir.git &
+pipx install git+https://github.com/andreafrancia/trash-cli.git &
+pipx install git+https://gitlab.com/ajak/tuir.git &
 pipx install twine &
 pipx install ueberzug &
-pipx install unimatrix --spec git+https://github.com/will8211/unimatrix.git
-pipx install urlscan --spec git+https://github.com/firecat53/urlscan.git &
+pipx install git+https://github.com/will8211/unimatrix.git
 pipx install virtualenv &
 pipx install xenon &
-pipx install xonsh --spec git+https://github.com/xonsh/xonsh.git
-pipx install youtube-dl --spec git+https://github.com/ytdl-org/youtube-dl.git
+pipx install git+https://github.com/xonsh/xonsh.git
+pipx install git+https://github.com/ytdl-org/youtube-dl.git
 pipx install mercurial
