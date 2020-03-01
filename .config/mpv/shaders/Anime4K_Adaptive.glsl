d8ce71587b55dd4556c30e639253eb7658466ff8
Rohan Kumar <seirdy@protonmail.com>
2020-03-01 00:47:17 -0800

    mpv: update to Anime4K 2.0

--- .config/mpv/shaders/Anime4K_Adaptive.glsl
+++
@@ -1,1 +0,0 @@
-/home/rkumar/Executables/ghq/github.com/bloc97/Anime4K/glsl/Anime4K_Adaptive_v1.0RC2.glsl
---
+++ .config/mpv/shaders/Anime4K_Hybrid_Fast.glsl
@@ -0,0 +1,1 @@
+/home/rkumar/Executables/ghq/github.com/bloc97/Anime4K/glsl/Anime4K_Hybrid_v2.0RC2_Fast.glsl
