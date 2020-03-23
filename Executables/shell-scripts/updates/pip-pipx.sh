726361d5e2e6c8f151a98a57cc11b6ce48361651
Rohan Kumar <seirdy@protonmail.com>
2020-03-23 14:24:33 -0700

    Fix comic-dl

--- Executables/shell-scripts/updates/pip-pipx.sh
+++ Executables/shell-scripts/updates/pip-pipx.sh
@@ -6,6 +6,19 @@ pip3 install -U --user git+https://github.com/pipxproject/pipx.git
 pip3 install -U --user pynvim
 pipx upgrade-all
 
+echo "updating comic-dl"
+# shellcheck disable=SC2154
+comicdl_dir="$GHQ_ROOT/github.com/Xonshiz/comic-dl"
+
+# shellcheck source=/dev/null
+ghq_get_cd https://github.com/Xonshiz/comic-dl.git \
+	&& {
+		[ -d "$comicdl_dir/venv" ] || virtualenv "$comicdl_dir/venv"
+	} \
+	&& . "$comicdl_dir/venv/bin/activate" \
+	&& python -m pip install -Ur "$comicdl_dir/requirements.txt" \
+	&& echo "successfully updated comic-dl"
+
 end_time=$(date '+%s')
 elapsed=$(echo "$end_time - $start_time" | bc)
 echo "Time elapsed: $elapsed seconds"
